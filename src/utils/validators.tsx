0f49c57d8543974a7738aeecfcac2b00676242ff
Ronny Yabar <ronnycontacto@gmail.com>
2023-08-01 16:58:51 -0500

    feat: add email and phone validators for the forms.

--- src/utils/validators.tsx
+++ src/utils/validators.tsx
@@ -1,3 +1,9 @@
-export const isValidPhoneNumber = (phoneNumber: string) => {
-  return /^\d{10}$/.test(phoneNumber);
+export const isValidPhoneNumber = (phoneNumber: string): boolean => {
+  const phoneNumberRegex = /^\d{3}-\d{3}-\d{4}$/;
+  return phoneNumberRegex.test(phoneNumber);
+};
+
+export const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 };
