31987a82186577ead0b1c114f24d7b759d942365
Ronny Yabar <ronnycontacto@gmail.com>
2023-08-01 16:58:01 -0500

    feat: add StickyBar component to the bottom of the right column.

---
+++ src/components/StickyBar.tsx
@@ -0,0 +1,31 @@
+import { StickyBarProps } from "../types/step.type";
+
+const StickyBar: React.FC<StickyBarProps> = ({
+  step,
+  numSteps,
+  handleNext,
+  handleBack,
+  isFormValid,
+}) => {
+  return (
+    <div className="sticky-bottom-bar">
+      <button
+        type="button"
+        onClick={handleBack}
+        className={`go-back-button ${step.key === 1 ? "no-display" : ""}`}
+      >
+        Go Back {step.key}
+      </button>
+      <button
+        type="button"
+        onClick={handleNext}
+        disabled={!isFormValid}
+        className={`save-continue-button ${step.key === numSteps ? "no-display" : ""}`}
+      >
+        Save and Continue
+      </button>
+    </div>
+  );
+};
+
+export default StickyBar;
